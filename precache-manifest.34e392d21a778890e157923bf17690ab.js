90b08c778c9dacb7365cade89ccd89a549074f2e
AsefHossainKhan <56684655+AsefHossainKhan@users.noreply.github.com>
2020-10-04 18:58:28 +0600

    Updates

---
+++ precache-manifest.34e392d21a778890e157923bf17690ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daf4b42e260cca89d18a9f6001786eb4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "939533fbc24ada060173",
+    "url": "/monsters-rolodex/static/css/main.f34c2e99.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "939533fbc24ada060173",
+    "url": "/monsters-rolodex/static/js/main.e4bf518c.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
